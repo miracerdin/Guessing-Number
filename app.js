d4fc3021996e7334e3516b7ac727782f3441e069
mirac <miracerdin@gmail.com>
2022-06-09 19:03:15 +0300

    Enter key activated

--- app.js
+++ app.js
@@ -10,56 +10,69 @@ const wrong = document.querySelector("#wrong");
 const correct = document.querySelector("#correct");
 
 let remainingLives = 5;
-guess.addEventListener("click", () => {
-  while (remainingLives > 0) {
-    if (input.value == number) {
-      document.querySelector(".border").classList.remove("bg-danger");
-      document.querySelector(".border").classList.add("bg-success");
-      correct1();
-      return (
-        (document.querySelector(".border").innerText =
-          "Congratulations! You know"),
-        (remainingLives = 0)
-      );
-    } else if (input.value < number) {
-      document.querySelector(".border").classList.add("bg-danger");
-      remainingLives--;
-      wrong1();
-      if (remainingLives == 0) {
-        return (document.querySelector(
-          ".border"
-        ).innerText = `Lost  number is: ${number}`);
-      } else {
-        return (document.querySelector(
-          ".border"
-        ).innerText = `Low ⬆ ${remainingLives}`);
-      }
-    } else {
-      remainingLives--;
-      wrong1();
-      if (remainingLives == 0) {
-        return (document.querySelector(
-          ".border"
-        ).innerText = `Lost  number is: ${number}`);
-      } else {
-        document.querySelector(".border").classList.add("bg-danger");
 
-        return (document.querySelector(
-          ".border"
-        ).innerText = `high ⬇ ${remainingLives}`);
-      }
-    }
+input.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementsByClassName("btn-primary")[0].click();
   }
 });
 
-tryit.addEventListener("click", () => {
-  window.location.reload(false);
-});
+function mirac() {
+  guess.addEventListener("click", () => {
+    while (remainingLives > 0) {
+      if (input.value == number) {
+        document.querySelector(".border").classList.remove("bg-danger");
+        document.querySelector(".border").classList.add("bg-success");
+        correct1();
+        return (
+          (document.querySelector(".border").innerText =
+            "Congratulations! You know"),
+          (remainingLives = 0)
+        );
+      } else if (input.value < number) {
+        document.querySelector(".border").classList.add("bg-danger");
+        remainingLives--;
+        wrong1();
+        if (remainingLives == 0) {
+          return (document.querySelector(
+            ".border"
+          ).innerText = `Lost  number is: ${number}`);
+        } else {
+          return (document.querySelector(
+            ".border"
+          ).innerText = `Low ⬆ ${remainingLives}`);
+        }
+      } else {
+        remainingLives--;
+        wrong1();
+        if (remainingLives == 0) {
+          return (document.querySelector(
+            ".border"
+          ).innerText = `Lost  number is: ${number}`);
+        } else {
+          document.querySelector(".border").classList.add("bg-danger");
 
-function correct1() {
-  correct.play();
-}
+          return (document.querySelector(
+            ".border"
+          ).innerText = `high ⬇ ${remainingLives}`);
+        }
+      }
+    }
+  });
 
-function wrong1() {
-  wrong.play();
+  tryit.addEventListener("click", () => {
+    window.location.reload(false);
+  });
+
+  function correct1() {
+    correct.play();
+  }
+
+  function wrong1() {
+    wrong.play();
+  }
 }
